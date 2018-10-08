97425f04df988b081bac654886d0d3b787bb7df4
Ben Spain <benjamin.spain@target.com>
2018-10-08 14:02:55 -0500

    Lint fixes

--- src/components/signup/SignUpForm.js
+++ src/components/signup/SignUpForm.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import SignUpFormError from './SignUpFormError';
 
 const SignUpForm = ({guest, errors, onSave, onChange, submitEnabled}) => {
--- src/components/signup/SignUpPage.js
+++ src/components/signup/SignUpPage.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { Redirect } from 'react-router-dom';
 import SignUpForm from './SignUpForm';
 
